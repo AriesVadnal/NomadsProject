0c34bf1112252bc996112ded47c078ad69c011a2
AriesVadnal <ariessatriawan89@gmail.com>
2021-02-09 18:26:33 +0700

    menmabah project

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+
+
+// const Startdetail = (props) => {
+//    return (
+//     <div className="stats-detail">
+//         <h2>{props.price}</h2>
+//         <p>{props.mem}</p>
+//     </div>
+//    )
+// }
+
+// class Start extends React.Component {
+//     render(){
+//         return(
+//             <div>
+//               <Startdetail price="20K" mem="Members"/>
+//             <Startdetail price="12K" mem="Countries"/>
+//             <Startdetail price="3K" mem="Hotels"/>
+//             <Startdetail price="72" mem="Partners"/>
+//             </div>
+//         )
+//     }
+// }
+
+
+
+// ReactDOM.render(<Start/>,document.getElementById("start"));
